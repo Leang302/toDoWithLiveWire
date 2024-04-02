40d55dd5a26309c100fab9f9c7435372a94d9011
leang <leangk49@gmail.com>
2024-04-02 09:04:48 +0700

    completed checkbox

--- resources/views/livewire/includes/todo-list-card.blade.php
+++ resources/views/livewire/includes/todo-list-card.blade.php
@@ -1,6 +1,8 @@
 <div wire:key='{{ $todo->id }}' style="border:1px black;">
+    <input type="checkbox" @checked($todo->completed)>
     <p>{{ $todo->name }}</p>
     <p>{{ $todo->created_at }}</p>
     <button>Edit</button>
     <button wire:click="delete({{ $todo->id }})">Delete</button>
+    <hr>
 </div>
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -3,7 +3,7 @@
 
     <p>Search</p>
     <input wire:model.live.debounce.500ms='search' type="text">
-
+    <hr>
     @foreach ($todos as $todo)
         @include('livewire.includes.todo-list-card')
     @endforeach
